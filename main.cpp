1d28c729f159d13aa7c745d7187f3345a27dae34
Pablo Sanabria <psanabria@uc.cl>
2017-05-09 16:42:30 -0300

    Added boost libraries

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <flann/flann.hpp>
+#include <boost/filesystem.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
 }
